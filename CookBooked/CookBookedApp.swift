edb1a0482f6b0c6887c340b8492bf10200df1dd6
kubinoes <j.schovanec@protonmail.com>
2023-06-08 20:10:38 +0200

    Initial Commit

---
+++ CookBooked/CookBookedApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookBookedApp.swift
+//  CookBooked
+//
+//  Created by Jakub Schovanec on 08/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CookBookedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
